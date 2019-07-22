94305bdeed0dc241b75c96c731f51d7f52d9298a
root <root@vps668853.ovh.net>
2019-07-22 08:06:40 +0200

    Clean setup with working CMake and Make

---
+++ bin/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/DependInfo.cmake
@@ -0,0 +1,82 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/root/repos/sleepy-discord-vcmp-client/sleepy-discord/sleepy_discord/json.c" "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/json.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CURL_STATICLIB"
+  "NONEXISTANT_GIT_INFO"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../sleepy-discord/include"
+  "../sleepy-discord/deps/include"
+  "../sleepy-discord/include/sleepy_discord"
+  "../sleepy-discord/deps/cpr/include"
+  "../sleepy-discord/deps/asio/asio/include"
+  "../sleepy-discord/deps/websocketpp"
+  "../sleepy-discord/include/sleepy_discord/IncludeNonexistent"
+  "../sleepy-discord/deps/cpr/opt/curl/include"
+  "sleepy-discord/deps/cpr/opt/curl/include/curl"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/repos/sleepy-discord-vcmp-client/sleepy-discord/sleepy_discord/asignments_client.cpp" "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/asignments_client.cpp.o"
+  "/root/repos/sleepy-discord-vcmp-client/sleepy-discord/sleepy_discord/attachment.cpp" "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/attachment.cpp.o"
+  "/root/repos/sleepy-discord-vcmp-client/sleepy-discord/sleepy_discord/channel.cpp" "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/channel.cpp.o"
+  "/root/repos/sleepy-discord-vcmp-client/sleepy-discord/sleepy_discord/client.cpp" "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/client.cpp.o"
+  "/root/repos/sleepy-discord-vcmp-client/sleepy-discord/sleepy_discord/cpr_session.cpp" "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/cpr_session.cpp.o"
+  "/root/repos/sleepy-discord-vcmp-client/sleepy-discord/sleepy_discord/default_functions.cpp" "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/default_functions.cpp.o"
+  "/root/repos/sleepy-discord-vcmp-client/sleepy-discord/sleepy_discord/discord_object_interface.cpp" "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/discord_object_interface.cpp.o"
+  "/root/repos/sleepy-discord-vcmp-client/sleepy-discord/sleepy_discord/embed.cpp" "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/embed.cpp.o"
+  "/root/repos/sleepy-discord-vcmp-client/sleepy-discord/sleepy_discord/endpoints.cpp" "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/endpoints.cpp.o"
+  "/root/repos/sleepy-discord-vcmp-client/sleepy-discord/sleepy_discord/gateway.cpp" "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/gateway.cpp.o"
+  "/root/repos/sleepy-discord-vcmp-client/sleepy-discord/sleepy_discord/http.cpp" "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/http.cpp.o"
+  "/root/repos/sleepy-discord-vcmp-client/sleepy-discord/sleepy_discord/invite.cpp" "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/invite.cpp.o"
+  "/root/repos/sleepy-discord-vcmp-client/sleepy-discord/sleepy_discord/json_wrapper.cpp" "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/json_wrapper.cpp.o"
+  "/root/repos/sleepy-discord-vcmp-client/sleepy-discord/sleepy_discord/message.cpp" "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/message.cpp.o"
+  "/root/repos/sleepy-discord-vcmp-client/sleepy-discord/sleepy_discord/permissions.cpp" "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/permissions.cpp.o"
+  "/root/repos/sleepy-discord-vcmp-client/sleepy-discord/sleepy_discord/sd_error.cpp" "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/sd_error.cpp.o"
+  "/root/repos/sleepy-discord-vcmp-client/sleepy-discord/sleepy_discord/server.cpp" "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/server.cpp.o"
+  "/root/repos/sleepy-discord-vcmp-client/sleepy-discord/sleepy_discord/user.cpp" "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/user.cpp.o"
+  "/root/repos/sleepy-discord-vcmp-client/sleepy-discord/sleepy_discord/uwebsockets_websocket.cpp" "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/uwebsockets_websocket.cpp.o"
+  "/root/repos/sleepy-discord-vcmp-client/sleepy-discord/sleepy_discord/voice.cpp" "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/voice.cpp.o"
+  "/root/repos/sleepy-discord-vcmp-client/sleepy-discord/sleepy_discord/webhook.cpp" "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/webhook.cpp.o"
+  "/root/repos/sleepy-discord-vcmp-client/sleepy-discord/sleepy_discord/websocketpp_websocket.cpp" "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/sleepy_discord/CMakeFiles/sleepy-discord.dir/websocketpp_websocket.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CURL_STATICLIB"
+  "NONEXISTANT_GIT_INFO"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../sleepy-discord/include"
+  "../sleepy-discord/deps/include"
+  "../sleepy-discord/include/sleepy_discord"
+  "../sleepy-discord/deps/cpr/include"
+  "../sleepy-discord/deps/asio/asio/include"
+  "../sleepy-discord/deps/websocketpp"
+  "../sleepy-discord/include/sleepy_discord/IncludeNonexistent"
+  "../sleepy-discord/deps/cpr/opt/curl/include"
+  "sleepy-discord/deps/cpr/opt/curl/include/curl"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/deps/cpr/cpr/CMakeFiles/cpr.dir/DependInfo.cmake"
+  "/root/repos/sleepy-discord-vcmp-client/bin/sleepy-discord/deps/cpr/opt/curl/lib/CMakeFiles/libcurl.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
